46db1239eaf6eb843851868b2d33856153107c38
wiemkhedhri <wiem.khedri50@gmail.com>
2025-01-06 22:30:05 +0100

    Remove cabinetmedicale folder from doccurefrontend branch

--- cabinetmedicale/src/main/java/com/example/pfefinale/repositery/AdminRepositery.java
+++
@@ -1,9 +0,0 @@
-package com.example.pfefinale.repositery;
-
-import com.example.pfefinale.models.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdminRepositery extends JpaRepository<Admin,Long> {
-}
--- cabinetmedicale/src/main/java/com/example/pfefinale/repositery/SpecialiteRepositery.java
+++
@@ -1,9 +0,0 @@
-package com.example.pfefinale.repositery;
-
-import com.example.pfefinale.models.Specialite;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SpecialiteRepositery extends JpaRepository<Specialite,Long> {
-}
